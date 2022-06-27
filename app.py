42ef055608142c1ab3ae17ea1692144660b09a95
VikiWahyudi05 <vickywahyudi05@gmail.com>
2022-06-27 14:48:15 +0700

    memperbaiki deploy

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ class ContohResource(Resource):
 api.add_resource(ContohResource, "/api", methods=['POST', 'GET'])
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5005)
